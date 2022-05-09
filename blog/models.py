4f03a1607c8b5e67a63f3413b01dc01538ed5f60
Mellany Linhares <mellinhares6@gmail.com>
2022-05-09 14:08:48 -0300

    feat: Add models User, Post e Comentario

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,54 @@
+from django.conf import settings
+from django.contrib.auth.base_user import BaseUserManager, AbstractBaseUser
+from django.contrib.auth.hashers import make_password
 from django.db import models
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password):
+        user = self.model(email=self.normalize_email(email))
+        user.password = make_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password=None):
+        user = self.create_user(email=email, password=password)
+        user.is_staff = True
+        user.is_active = True
+        user.save()
+        return user
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(verbose_name="Email", unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False, verbose_name="Administrador")
+
+    USERNAME_FIELD = "email"
+
+    objects = UserManager()
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    class Meta:
+        verbose_name = "Usuário"
+        verbose_name_plural = "Usuários"
+
+
+class Post(models.Model):
+    titulo = models.CharField(max_length=300)
+    conteudo = models.TextField()
+    autor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='posts')
+    curtidas = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.titulo
+
+
+class Comentario(models.Model):
+    nome_usuario = models.CharField(max_length=255)
+    conteudo = models.CharField(max_length=500)
